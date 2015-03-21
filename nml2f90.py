29f6449becab332366f123b9e86a7a8db5cfbfda
perrette <mahe.perrette@pik-potsdam.de>
2015-03-21 04:24:39 +0100

    added --group-mapping optional parameter to rename types

--- nml2f90.py
+++ nml2f90.py
@@ -5,7 +5,7 @@ This script will read the namelist.nml input file and output ioparams.f90, that
 contains corresponding parameter types and I/O, setter/getter routines.
 
 Usage:
-    nml2f90.py <namelist.nml> [<ioparams>] [--clen <clen>] [--aclen <aclen>] [--full]
+    nml2f90.py <namelist.nml> [<ioparams>] [--clen <clen>] [--aclen <aclen>] [--full] [--group-mapping MAP]
 
 Options:
     -h --help       Show this screen
@@ -14,8 +14,9 @@ Options:
     --clen CLEN     indicate the length of character strings  [ default: 256 ]
     --aclen ACLEN   indicate the length of character strings in arrays  [ default: 256]
     --full          do include get_param, set_param functions
+    --group-mapping MAP mapping from group to type name (dict as json format)
 """
-import sys, os
+import sys, os, json
 from collections import OrderedDict as odict
 from namelist import Namelist, read_namelist_file
 import nml2f90_templates
@@ -435,6 +436,14 @@ if __name__ == "__main__":
         if args['--aclen']: aclen = int(args['--aclen'])
         if args['--full']:
             include_setget = True
+        if args['--group-mapping']:
+            mapping = json.loads(args['--group-mapping'])
+            der_orig = derived_type_name
+            def derived_type_name(group):
+                if group in mapping:
+                    return mapping[group]
+                else:
+                    return der_orig(group)
 
     if io_mod.endswith(".f90"):
         io_file = io_mod  
