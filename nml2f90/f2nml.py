eeadfe9fac685a0c333d2212f093ee70c0f30a83
perrette <mahe.perrette@pik-potsdam.de>
2015-04-23 19:48:38 +0200

    add header to created namelist

--- nml2f90/f2nml.py
+++ nml2f90/f2nml.py
@@ -5,15 +5,17 @@ This script will parse some fortran source code and write a namelist file,
 provided a list of namelist groups and their mapping as type name.
 """
 from __future__ import print_function
-import os
+import os, sys
 import json
 import argparse
 import warnings
+import datetime
 from itertools import groupby
 from .namelist import Namelist, Param
 from . import ioparams
 from .ioparams import Group, Variable
 from .parsef90 import parse_type
+from .version import version as __version__
 
 def _get_type_name(group_name, type_map, type_prefix, type_suffix):
     " build type name from group name "
@@ -58,8 +60,23 @@ def main():
         group = parse_type(code, type_name=type_name, group_name=g)
         nml.extend(group.to_nml())
 
+    # convert to string
+    stringnml = nml.format()
+
+    # add header
+    header = ""
+    header += "! ======================================================================\n"
+    header += "! Namelist originally generated from source code using `f2nml` utility \n"
+    header += "! Download : https://github.com/perrette/nml-to-f90\n"
+    header += "! Version : {}\n".format(__version__)
+    header += "! Date : {}\n".format(datetime.datetime.today()) # parsed file (e.g. namelist) for the generation of the module
+    header += "! Command : {}\n".format(" ".join(sys.argv))
+    header += "! ======================================================================\n\n"
+    stringnml = header + stringnml
+
     # Write to file
-    nml.write(args.nml)
+    with open(args.nml,'w') as f:
+        f.write(stringnml)
 
     print("done.")
 
